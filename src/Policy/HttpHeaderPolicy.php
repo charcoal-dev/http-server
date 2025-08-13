5a6aa8c698b90d2b5c36b45b6dbfa976d9553f7f
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-13 23:25:50 +0500

    Add HttpPayloadPolicy and HttpHeaderPolicy classes
    Introduce `HttpPayloadPolicy` and `HttpHeaderPolicy` classes as extensions of `HttpDataPolicy`. These classes define specific policies for handling HTTP payloads and headers with configurable validation and charset settings.

---
+++ src/Policy/HttpHeaderPolicy.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Part of the "charcoal-dev/http-router" package.
+ * @link https://github.com/charcoal-dev/http-router
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Http\Router\Policy;
+
+use Charcoal\Base\Enums\Charset;
+use Charcoal\Base\Enums\ValidationState;
+use Charcoal\Http\Commons\Data\HttpDataPolicy;
+use Charcoal\Http\Commons\Enums\HttpHeaderKeyPolicy;
+
+/**
+ * Class HttpHeaderPolicy
+ * @package Charcoal\Http\Router\Policy
+ */
+readonly class HttpHeaderPolicy extends HttpDataPolicy
+{
+    public function __construct(
+        public HttpHeaderKeyPolicy $keyPolicy = HttpHeaderKeyPolicy::STRICT,
+        int                        $keyMaxLength = 64,
+        bool                       $keyOverflowTrim = false,
+        int                        $valueMaxLength = 2048,
+        bool                       $valueOverflowTrim = false,
+        ValidationState            $accessKeyTrust = ValidationState::VALIDATED,
+        ValidationState            $setterKeyTrust = ValidationState::RAW,
+        ValidationState            $valueTrust = ValidationState::RAW,
+    )
+    {
+        parent::__construct(
+            charset: Charset::ASCII,
+            keyMaxLength: $keyMaxLength,
+            keyOverflowTrim: $keyOverflowTrim,
+            valueMaxLength: $valueMaxLength,
+            valueOverflowTrim: $valueOverflowTrim,
+            accessKeyTrust: $accessKeyTrust,
+            setterKeyTrust: $setterKeyTrust,
+            valueTrust: $valueTrust,
+        );
+    }
+}
