a22d4b61889815d0b18254e4b60f600df4eb525d
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-13 23:57:06 +0500

    Standardize docblock format in `HttpServer` for consistency and readability

--- src/HttpServer.php
+++ src/HttpServer.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/http-router" package.
- * https://github.com/charcoal-dev/http-router
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/http-router/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/http-router" package.
+ * @link https://github.com/charcoal-dev/http-router
  */
 
 declare(strict_types=1);
