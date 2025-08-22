7f13d620b76754f8b522eedc2adc6db443fee88a
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-22 22:07:45 +0500

    Add `HttpMethods` class for managing HTTP method collections
    - Introduced `HttpMethods` extending `AbstractEnumVector` for handling collections of `HttpMethod` enumerations.

---
+++ src/Support/HttpMethods.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Part of the "charcoal-dev/http-router" package.
+ * @link https://github.com/charcoal-dev/http-router
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Http\Router\Support;
+
+use Charcoal\Base\Vectors\AbstractEnumVector;
+use Charcoal\Http\Commons\Enums\HttpMethod;
+
+/**
+ * Represents a collection of HTTP methods as an enumeration vector.
+ */
+class HttpMethods extends AbstractEnumVector
+{
+    public function __construct(HttpMethod ...$methods)
+    {
+        parent::__construct(...$methods);
+    }
+}
