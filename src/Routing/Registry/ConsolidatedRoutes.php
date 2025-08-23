a55f8762d126eb95118934650758b9f47f2cc7a3
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-23 15:38:41 +0500

    Refactor `ConsolidatedRoutes` to include controller validation and snapshot creation:
    - Added `controllers` and `entryPoints` properties for structured route and controller mapping.
    - Introduced route snapshot creation with `ControllerBinding` and `RouteSnapshot` integration.
    - Improved duplicate route validation logic for enhanced error handling.

--- src/Routing/Registry/ConsolidatedRoutes.php
+++ src/Routing/Registry/ConsolidatedRoutes.php
@@ -16,15 +16,21 @@ use Charcoal\Http\Router\Routing\AppRoutes;
 use Charcoal\Http\Router\Routing\Group\AbstractRouteGroup;
 use Charcoal\Http\Router\Routing\Route;
 use Charcoal\Http\Router\Routing\Snapshot\AppRoutingSnapshot;
+use Charcoal\Http\Router\Routing\Snapshot\ControllerBinding;
+use Charcoal\Http\Router\Routing\Snapshot\RouteSnapshot;
 
 /**
  * Represents a consolidated view of application routes.
  * Provides utilities to handle and organize routes into a structured format.
  */
-final class ConsolidatedRoutes
+final readonly class ConsolidatedRoutes
 {
     /** @var array<string, list<Route|AbstractRouteGroup>> */
     public array $declared;
+    /** @var array<class-string<ControllerInterface>,ControllerValidated> */
+    public array $controllers;
+    /** @var array<string,<array<string,class-string<ControllerInterface>>> */
+    public array $entryPoints;
 
     /**
      * @param AbstractRouteGroup $group
@@ -47,41 +53,49 @@ final class ConsolidatedRoutes
         $routeMap = [];
         /** @var array<class-string<ControllerInterface>,array<string>> $controllers */
         $controllers = [];
-        foreach ($this->declared as $path => $route) {
-            if (!$route instanceof Route) {
-                continue;
-            }
-
-            if (!isset($routeMap[$path])) {
-                $routeMap[$path] = [];
-            }
-
-            if (!isset($controllers[$route->classname])) {
-                $controllers[$route->classname] = [];
-            }
-
-            $methods = array_map(fn($m) => $m->name ?? [], $route->methods?->getArray() ?? []);
-
-            // Append controllers map
-            $controllers[$route->classname] = [...$controllers[$route->classname], ...$methods];
-
-            // Set the wildcard method for the route map
-            if (!$methods) {
-                $methods = [Constants::METHOD_ANY];
-            }
-
-            foreach ($methods as $method) {
-                if (isset($routeMap[$path][$method])) {
-                    throw new \InvalidArgumentException("Duplicate route: " . $path . " " . $method);
+        foreach ($this->declared as $path => $routes) {
+            foreach ($routes as $route) {
+                if (!$route instanceof Route) {
+                    continue;
                 }
 
-                $routeMap[$path][$method] = $route->classname;
+                if (!isset($routeMap[$path])) {
+                    $routeMap[$path] = [];
+                }
+
+                if (!isset($controllers[$route->classname])) {
+                    $controllers[$route->classname] = [];
+                }
+
+                $methods = array_map(fn($m) => $m->name ?? [], $route->methods?->getArray() ?? []);
+
+                // Append to be validated controller entryPoints
+                $controllers[$route->classname] = [...$controllers[$route->classname], ...$methods];
+
+                // Set the wildcard method for the route map
+                if (!$methods) {
+                    $methods = [Constants::METHOD_ANY];
+                }
+
+                // Don't allow duplicate methods+paths
+                foreach ($methods as $method) {
+                    // Deduplicate routes
+                    if (isset($routeMap[$path][$method])) {
+                        throw new \InvalidArgumentException("Duplicate route: " . $path . " " . $method);
+                    }
+
+                    $routeMap[$path][$method] = $route->classname;
+                }
             }
         }
 
         // All entrypoint are now known!
         $validatedControllers = [];
         foreach ($controllers as $classname => $methods) {
+            if (isset($validatedControllers[$classname])) {
+                continue;
+            }
+
             $validatedControllers[$classname] = new ControllerValidated(
                 $classname,
                 $methods,
@@ -90,12 +104,38 @@ final class ConsolidatedRoutes
         }
 
         // Controllers are now validated!
-
+        $this->controllers = $validatedControllers;
+        $this->entryPoints = $routeMap;
     }
 
+    /**
+     * Creates a snapshot of the currently declared routes along with their bindings.
+     *
+     * @return AppRoutingSnapshot The snapshot containing all route paths and their associated controller bindings.
+     */
     public function snapshot(): AppRoutingSnapshot
     {
+        // Create a final binding map
+        $snapshot = [];
+        foreach ($this->declared as $path => $routes) {
+            /** @var array<ControllerBinding> $routeMap */
+            $pathBindings = [];
+            foreach ($routes as $route) {
+                if (!$route instanceof Route) {
+                    continue;
+                }
 
+                $pathBindings[] = new ControllerBinding(
+                    $route->classname,
+                    $route->methods?->getArray() ?? true,
+                    $route->middleware
+                );
+            }
+
+            $snapshot[] = new RouteSnapshot($path, ...$pathBindings);
+        }
+
+        return new AppRoutingSnapshot(...$snapshot);
     }
 
     /**
