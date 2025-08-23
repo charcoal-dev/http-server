59f9b0e6912ecc588b6141f4ffdea2b4288f7f04
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-23 20:24:45 +0500

    Rename `MiddlewareFqcnWhitelistInterface` to `MiddlewareTrustPolicyInterface`, refine trust policy logic, and add `isTrusted` method with extended middleware support.

---
+++ src/Contracts/Middleware/MiddlewareTrustPolicyInterface.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Part of the "charcoal-dev/http-router" package.
+ * @link https://github.com/charcoal-dev/http-router
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Http\Router\Contracts\Middleware;
+
+use Charcoal\Http\Router\Contracts\Middleware\Group\GroupMiddlewareInterface;
+use Charcoal\Http\Router\Enums\Middleware\Scope;
+
+/**
+ * Defines a contract for implementing a trust policy for middleware handling.
+ * Implementations can be as simple as a hard-coded allowlist of middleware FQCNs,
+ * or consult config/env to allow more in dev and fewer in prod. Keep it fast and
+ * deterministic (no I/O).
+ */
+interface MiddlewareTrustPolicyInterface
+{
+    /**
+     * Checks if the provided middleware is accepted for the given scope.
+     * @param class-string<MiddlewareInterface> $middleware
+     * @param Scope $scope
+     * @return bool
+     * @api
+     */
+    public function isTrustedFqcn(string $middleware, Scope $scope): bool;
+
+    /**
+     * Determines whether the given middleware is trusted based on the provided scope.
+     * @param GlobalMiddlewareResolverInterface|GroupMiddlewareInterface|RouteMiddlewareInterface $middleware
+     * @param Scope $scope
+     * @return bool
+     * @api
+     */
+    public function isTrusted(
+        GlobalMiddlewareResolverInterface|GroupMiddlewareInterface|RouteMiddlewareInterface $middleware,
+        Scope                                                                               $scope
+    ): bool;
+}
