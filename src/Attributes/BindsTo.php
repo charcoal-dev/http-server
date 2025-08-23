712102fca1fe16e3009d2eefa16a6ad43e958ca1
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-23 13:55:46 +0500

    Improve `BindsTo` attribute with detailed docblock and scoped `Attribute` usage

--- src/Attributes/BindsTo.php
+++ src/Attributes/BindsTo.php
@@ -8,9 +8,11 @@ declare(strict_types=1);
 
 namespace Charcoal\Http\Router\Attributes;
 
-use Attribute;
-
-#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+/**
+ * An attribute class that binds a class to a specified contract.
+ * This attribute is used to associate a class with an interface or abstract class that it implements or extends.
+ */
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 final readonly class BindsTo
 {
     /** @param class-string $contract */
