dbeabe18e25732ea73bd3e35a5d9c4278180eb30
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-23 15:42:36 +0500

    Add `Constants` class for centralizing library-wide constant values

---
+++ src/Internal/Constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the "charcoal-dev/http-router" package.
+ * @link https://github.com/charcoal-dev/http-router
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Http\Router\Internal;
+
+/**
+ * This class contains constant values used across the lib.
+ */
+class Constants
+{
+    public const string METHOD_ANY = "(*)";
+}
