f1cccbda294667b85d4b6e562a8ffd91eb3b9017
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-26 14:58:55 +0500

    Refactor `UrlValidator` to replace `RequestConstraints` with `maxUriBytes` parameter and update exception message.

--- src/Pipelines/UrlValidator.php
+++ src/Pipelines/UrlValidator.php
@@ -9,13 +9,13 @@ declare(strict_types=1);
 namespace Charcoal\Http\Server\Pipelines;
 
 use Charcoal\Http\Commons\Url\UrlInfo;
-use Charcoal\Http\Server\Config\RequestConstraints;
 use Charcoal\Http\Server\Contracts\Middleware\UrlValidatorPipeline;
 use Charcoal\Http\Server\Request\Result\RedirectUrl;
 
 /**
  * The UrlValidatorPipeline class provides functionality for validating URLs
  * and applying constraints to determine if a redirect URL can be resolved.
+ * @see UrlValidatorPipeline
  */
 final readonly class UrlValidator implements UrlValidatorPipeline
 {
@@ -28,10 +28,10 @@ final readonly class UrlValidator implements UrlValidatorPipeline
      * Handles the processing of a given URL and returns a redirect, if applicable,
      * based on the specified constraints and the path normalization rules.
      */
-    public function __invoke(UrlInfo $url, RequestConstraints $constraints): ?RedirectUrl
+    public function __invoke(UrlInfo $url, int $maxUriBytes): ?RedirectUrl
     {
-        if (strlen($url->complete) > $constraints->maxUriBytes) {
-            throw new \LengthException("URL exceed maximum length: " . $constraints->maxUriBytes);
+        if (strlen($url->complete) > $maxUriBytes) {
+            throw new \LengthException("URL exceed maximum length: " . $maxUriBytes);
         }
 
         if (!$url->path || $url->path === "/") {
