ac7e160c2e398feca3273753f88235b0e690be5b
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-26 08:19:20 +0500

    Remove `BindsTo` attribute and migrate `Router` attributes to `Server` namespace:
    - Delete `BindsTo` class.
    - Rename relevant namespaces from `Router` to `Server`.
    - Update package metadata to reflect the `http-server` migration.

--- src/Attributes/AllowedParam.php
+++ src/Attributes/AllowedParam.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Part of the "charcoal-dev/http-router" package.
- * @link https://github.com/charcoal-dev/http-router
+ * Part of the "charcoal-dev/http-server" package.
+ * @link https://github.com/charcoal-dev/http-server
  */
 
 declare(strict_types=1);
 
-namespace Charcoal\Http\Router\Attributes\Controllers;
+namespace Charcoal\Http\Server\Attributes;
 
 /**
  * Attribute class used to specify required parameters for methods.
