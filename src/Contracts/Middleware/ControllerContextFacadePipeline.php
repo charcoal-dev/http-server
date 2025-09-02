4fca5a7ffbb43f26a9ef3dc22e65b516c80e89e3
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-02 21:27:52 +0500

    Introduce `ControllerContextFacadePipeline` interface for controller-specific middleware
    - Define a facade to expose controlled context and streamline controller execution.
    - Establish `RequestFacade` and `RequestGateway` contract integration.

---
+++ src/Contracts/Middleware/ControllerContextFacadePipeline.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of the "charcoal-dev/http-server" package.
+ * @link https://github.com/charcoal-dev/http-server
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Http\Server\Contracts\Middleware;
+
+use Charcoal\Http\Server\Request\Controller\RequestFacade;
+use Charcoal\Http\Server\Request\RequestGateway;
+
+/**
+ * Declare a facade/context given to controllers for execution.
+ * Default RequestFace exposes the recommended amount of context to controllers.
+ */
+interface ControllerContextFacadePipeline extends PipelineMiddlewareInterface
+{
+    public function __invoke(RequestGateway $request): RequestFacade;
+}
