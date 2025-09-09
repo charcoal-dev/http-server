fc5aa9a8abdf91d4d9d9bdfd516fc96f3de147ab
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-09 16:25:39 +0500

    Implement `ExceptionTraceContextInterface` in `HostnamePortMismatchException` and update dependency version
    - Add `getTraceContext` method to provide exception trace context with hostname and port.
    - Update `charcoal-dev/contracts` to version `^0.1.18` in `composer.json`.

--- src/Exceptions/Request/HostnamePortMismatchException.php
+++ src/Exceptions/Request/HostnamePortMismatchException.php
@@ -8,10 +8,12 @@ declare(strict_types=1);
 
 namespace Charcoal\Http\Server\Exceptions\Request;
 
+use Charcoal\Contracts\Errors\ExceptionTraceContextInterface;
+
 /**
  * Exception thrown when a mismatch occurs between a hostname and a port.
  */
-final class HostnamePortMismatchException extends \Exception
+final class HostnamePortMismatchException extends \Exception implements ExceptionTraceContextInterface
 {
     public function __construct(
         public readonly string $hostname,
@@ -19,5 +21,16 @@ final class HostnamePortMismatchException extends \Exception
     )
     {
         parent::__construct("Hostname and port did not match");
+    }
+
+    /**
+     * @return array
+     */
+    public function getTraceContext(): array
+    {
+        return [
+            "hostname" => $this->hostname,
+            "port" => $this->port,
+        ];
     }
 }
